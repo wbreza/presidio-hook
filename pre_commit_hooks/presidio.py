1e4c5f0ac34266e591a0641e9830e03131912103
Wallace Breza <wallace.breza@microsoft.com>
2021-11-23 11:48:54 -0800

    Initial commit

---
+++ pre_commit_hooks/presidio.py
@@ -0,0 +1,29 @@
+from typing import Sequence
+
+
+from typing import Optional
+from typing import Sequence
+from presidio_analyzer import AnalyzerEngine
+from presidio_anonymizer import AnonymizerEngine
+import argparse
+
+def main(argv: Optional[Sequence[str]] = None) -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filenames', nargs='*')
+    args = parser.parse_args(argv)
+
+    # Set up the engine, loads the NLP module (spaCy model by default) 
+    # and other PII recognizers
+    analyzer = AnalyzerEngine()
+
+    text="My phone number is 212-555-5555"
+
+    results = analyzer.analyze(text=text,
+                           entities=["PHONE_NUMBER"],
+                           language='en')
+    print(results)
+
+    return 0
+
+if __name__ == '__main__':
+    raise SystemExit(main())
