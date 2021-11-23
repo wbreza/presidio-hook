c294e43d616d563610a636130ccf32099d9b8709
Wallace Breza <wallace.breza@microsoft.com>
2021-11-23 13:45:20 -0800

    Scan files

--- pre_commit_hooks/presidio.py
+++ pre_commit_hooks/presidio.py
@@ -4,26 +4,67 @@ from typing import Sequence
 from typing import Optional
 from typing import Sequence
 from presidio_analyzer import AnalyzerEngine
-from presidio_anonymizer import AnonymizerEngine
+from util import color
 import argparse
+import yaml
+import textwrap
+
 
 def main(argv: Optional[Sequence[str]] = None) -> int:
     parser = argparse.ArgumentParser()
     parser.add_argument('filenames', nargs='*')
+    parser.add_argument('--config', dest="config", help="The presidio configuration")
     args = parser.parse_args(argv)
 
-    # Set up the engine, loads the NLP module (spaCy model by default) 
+    # Set up the engine, loads the NLP module (spaCy model by default)
     # and other PII recognizers
     analyzer = AnalyzerEngine()
 
-    text="My phone number is 212-555-5555"
+    all_results = []
 
-    results = analyzer.analyze(text=text,
-                           entities=["PHONE_NUMBER"],
-                           language='en')
-    print(results)
+    print(args)
 
-    return 0
+    returnValue = 0
+    language = "en"
+    entities = ["PHONE_NUMBER"]
+
+    if args.config != "":
+        with open(args.config) as config:
+            config_data = yaml.load(config, yaml.FullLoader)
+            language = config_data["language"]
+            entities = config_data["entities"]
+
+    for filename in args.filenames:
+        with open(filename) as f:
+            text = f.read()
+            results = analyzer.analyze(text=text, entities=entities, language=language)
+            all_results.append(dict(filename=filename, results=results))
+
+    if len(all_results) > 0:
+        returnValue = 1
+
+        print(
+            textwrap.fill(
+                color.colorize(
+                    'ERROR: Potential sensitive data about to be committed to git repo!',
+                    color.AnsiColor.RED,
+                ),
+                width=80,
+            ),
+        )
+        print()
+
+        for fileResult in all_results:
+            print(textwrap.fill(
+                color.colorize(f'file: {fileResult["filename"]}', color.AnsiColor.BOLD)
+            ))
+            for result in fileResult["results"]:
+                print(textwrap.fill(
+                    color.colorize(f'\t{result}', color.AnsiColor.PURPLE)
+                ))
+
+    return returnValue
+
 
 if __name__ == '__main__':
     raise SystemExit(main())
